efa01ec54871b18be0da7000094e71e898a15fb0
pankaj-nipralo <pankaj.g@nipralo.com>
2025-07-25 18:14:15 +0530

    commit

--- frontend/src/components/landlord/PropertyListings/ListingMaster.jsx
+++ frontend/src/components/landlord/PropertyListings/ListingMaster.jsx
@@ -1,12 +1,12 @@
 import React from 'react' 
 import ListingBody from './ListingBody'
-import ButtonExamples from '@/components/common/Buttons/Button.examples.jsx'
+// import ButtonExamples from '@/components/common/Buttons/Button.examples.jsx'
 
 const ListingMaster = () => {
   return (
     <div className="min-h-screen p-6">
     <ListingBody />
-    <ButtonExamples />
+    {/* <ButtonExamples /> */}
     </div>
   )
 }
